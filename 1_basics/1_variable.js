3a913f6bf4d6b69da7bef32674c1dc6029b70467
Abdul Moiz <contactwithabdulmoiz@gmail.com>
2025-09-21 11:24:13 +0000

    Datatypes

---
+++ 1_basics/1_variable.js
@@ -0,0 +1,11 @@
+const accountId = 1458
+var accountEmail = "moiz@gmail.com"
+let accountPassword = "4859"
+accountCity = "GRW"
+
+//no use of var because of block scope and functional scope
+
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all JS code as new version
+
+//alert ( 3 + 3) //we are using Nodejs not browser
+
+let name = "moiz"
+let age = 22
+let subject = null
+
+// Primitive Datatypes
+//number  (max range) => 2 power 53
+//bigInt for large numbers
+//string => ""
+//boolean => true / False
+//Null =? standalone value 
+//undefined
+//symbol => unique
+
+//Object
+//null is a object
+
+console.log(typeof age)
+console.log(typeof null)
